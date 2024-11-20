39b37a44b9e71791ca0cbd9e6e844f59800155a6
angelchambi <angelchambi03@gmail.com>
2024-11-20 02:30:07 -0500

    BUCLES EJERCICIO 2

---
+++ BUCLES/ejercicio2.js
@@ -0,0 +1,27 @@
+// dias de la semana
+
+switch(OPCION = parseInt(prompt("Digite la opcion: "))){
+    case 1:
+        console.log("OPCION 1: LUNES");
+        break;
+    case 2:
+        console .log("OPCION 2: MARTES");
+        break;
+    case 3:
+         console.log("OPCION 3: MIERCOLES");
+        break;
+    case 4:
+        console.log("OPCION 4: JUEVESS");
+        break;
+    case 5:
+        console.log("OPCION 5: VIERNES");
+        break;
+    case 6:
+        console.log("OPCION 6: SABADO");
+        break;
+    case 7:
+        console.log("OPCION 7: DOMINGO");
+        break;
+    default:
+        console.log("OPCION NO VALIDO");
+}

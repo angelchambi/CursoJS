b53cce026eba2b3649db0dae94047b92b0f3b174
angelchambi <angelchambi03@gmail.com>
2024-11-20 01:54:13 -0500

    Ejercicio3 de condicionales

---
+++ CONDICIONALES/ejercicio3.js
@@ -0,0 +1,13 @@
+// comparacion de dos numeros : declara 2 variables a y b numericos y utiliza condicionales para comparar si a es mayor, menor o igual a b.
+
+const a = 30; // las primera variable
+const b = 30; // la segunda variable
+if (a>b){  // declaramos en if las comparacion de las variables
+    console.log("La variable A es: Mayor a B");        // declaramos en la consola si A es mayor a b, que digite es mayor
+    }
+    else if(a==b) { // declaramos en else if la variable igual de las 2 variables
+        console.log("La variable A es: Igual a B"); // declaramos a la consola que A es igual a B
+    } 
+    else { // si no cumpliera 
+        console.log("La variable A es: Menor a B"); // declaramos en la consola que la variable A es menor a b 
+    }

648f3d7994af0112df846baf95fe71056201959e
angelchambi <angelchambi03@gmail.com>
2024-11-20 02:18:57 -0500

    bucles ejercicio 1

---
+++ BUCLES/bucles.js
@@ -0,0 +1,16 @@
+// calculadora
+
+switch(opcion= parseInt(prompt("Digite la opcion: "))){ // se declara el bucle Switch para los casos
+    case 1:
+        console.log("OPCION 1: SUMAR ");
+        break;
+    case 2:
+        console.log("OPCION 2: RESTAR");
+        break;
+    case 3:
+        console.log("OPCION 3: MULTIPLICAR");
+        break;
+    default:
+        console.log("OPCION NO VALIDAD");
+}
+

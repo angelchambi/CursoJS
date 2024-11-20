92cca95e1a2106d2bc56561a7f397faf77f7a770
angelchambi <angelchambi03@gmail.com>
2024-11-19 23:07:53 -0500

    CONDICIONALES

---
+++ CONDICIONALES/ejercicio1.js
@@ -0,0 +1,15 @@
+// Declara tres variables a, b y c con valores numericos y utiliza condicionales para determinar cual de ellos es el mayor.
+
+const a= 10;
+const b= 25;
+const c= 15;
+
+if (a>b && a>c){
+    console.log("El mayor es: "+a);
+}
+if (b>a && b>c) {
+    console.log("El mayor es: "+b)
+} 
+else {
+    console.log("El mayor es: "+c) 
+}
